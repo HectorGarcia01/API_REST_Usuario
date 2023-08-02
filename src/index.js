f1a90de965b7373058feda5d9206cf782f446482
HectorGarcia01 <hg2779948@gmail.com>
2023-08-02 00:25:44 -0600

    Configuración de middleware para recibir la información y para habilitar la configuración de cors, además de agregar la ruta para usuarios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const usuarioRoutes = require('./routes/usuarios.routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Middleware
+app.use(express.json()); //Recibir información
+app.use(cors());        //Habilitar otras aplicaciones para realizar solicitudes a nuestra app
+
+//Agregando las rutas a la app
+app.use('/usuarios', usuarioRoutes);
+
+//Inicialización del servidor
 app.listen(port, () => {
     console.log(`Servidor inicializado en el puerto ${port}`);
 });
