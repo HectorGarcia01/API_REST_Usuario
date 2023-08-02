f5a8a7f0edcfca76e2da65017200d847f2992bfe
HectorGarcia01 <hg2779948@gmail.com>
2023-08-02 00:55:05 -0600

    Actualización

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-require('dotenv').config({ path: `.dev.env` })
+require('dotenv').config({ path: `.dev.env` });
 module.exports = {
     DATABASE: process.env.DATABASE,
     USERNAME: process.env.USERNAME,
     PASSWORD: process.env.PASSWORD,
     HOST: process.env.HOST
-}
+};
