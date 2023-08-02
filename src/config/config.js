e64316f83208a6447fa082779c097ed141b8a407
HectorGarcia01 <hg2779948@gmail.com>
2023-08-02 00:52:37 -0600

    Obteniendo y exportando todas las variables de entorno

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: `.dev.env` })
+module.exports = {
+    DATABASE: process.env.DATABASE,
+    USERNAME: process.env.USERNAME,
+    PASSWORD: process.env.PASSWORD,
+    HOST: process.env.HOST
+}
